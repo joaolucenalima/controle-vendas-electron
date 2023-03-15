affbab969359932f508facef87bc215e851b8765
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2023-03-15 12:39:02 -0300

    feat: função para verificar a existencia do repositório

--- src/main/main.ts
+++ src/main/main.ts
@@ -1,17 +1,27 @@
-const { app, BrowserWindow, nativeImage } = require('electron');
-const path = require('path')
-
+import { app, BrowserWindow, nativeImage } from 'electron'
+import path from 'path'
 import fs from 'fs'
+
 require("./main-events")
 
 const appPath = app.getAppPath()
 
+function verifyDatabaseDir() {
+  //Definindo o diretório do banco de dados
+  const database_dir = path.resolve(app.getPath("userData"), "database")
+
+  //Se a pasta não existir, ela é criada
+  if (!fs.existsSync(database_dir)) {
+    fs.mkdirSync(database_dir)
+  }
+}
+
 function CreateWindow() {
 
-  const icon = nativeImage.createFromPath('./build/icon.ico');
+  const icon = nativeImage.createFromPath('./build/icon.ico')
 
   if (app.dock) {
-    app.dock.setIcon(icon);
+    app.dock.setIcon(icon)
   }
 
   const mainWindow = new BrowserWindow({
@@ -36,9 +46,9 @@ app.on('ready', CreateWindow)
 
 app.on("window-all-closed", () => {
   if (process.platform !== "darwin") {
-    app.quit();
+    app.quit()
   }
-});
+})
 
 app.on('activate', () => {
   if (BrowserWindow.getAllWindows().length === 0) {
@@ -51,7 +61,10 @@ const isUnicWindow = app.requestSingleInstanceLock()
 if (!isUnicWindow) {
   app.quit()
 } else {
-  app.whenReady().then(CreateWindow)
+  app.whenReady().then(() => {
+    verifyDatabaseDir()
+    CreateWindow()
+  })
 }
 
 app.on('second-instance', () => {
