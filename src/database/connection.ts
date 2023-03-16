2b913a8589d8ac79359f55551a5b9a23271d03c1
João Vitor Lucena <joaovitorlucena000@gmail.com>
2023-03-16 10:45:54 -0300

    database removido da pasta AppData

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -1,13 +1,8 @@
-import { ipcRenderer } from "electron"
-import path from "path"
 import { Sequelize } from "sequelize"
-
-const database_dir = path.resolve(ipcRenderer.sendSync('get-data-path'), "database")
-const database_path = path.resolve(database_dir, "database.sqlite")
 
 const sequelize = new Sequelize({
   dialect: 'sqlite',
-  storage: database_path,
+  storage: './database.sqlite',
   logging: false,
 });
 
