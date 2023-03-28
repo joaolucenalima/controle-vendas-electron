5d3e775cb9c50bcff9ebf62beac47537869e2a44
João Vitor Lucena <joaovitorlucena000@gmail.com>
2023-03-28 12:41:53 -0300

    fix: erro com módulo em importação

--- forge.config.js
+++ forge.config.js
@@ -1,45 +1,45 @@
-import { version } from "./package.json";
-
-export const packagerConfig = {
-  icon: './build/icon',
-  ignore: [
-    "\\.git",
-    "\\.ts",
-    "\\.tsx",
-  ]
-};
-export const rebuildConfig = {};
-export const makers = [
-  {
-    name: '@electron-forge/maker-squirrel',
-    config: {
-      "setupIcon": "./build/icon.ico",
-      "setupExe": `controle-estoque-${version}.exe`,
-    },
+module.exports = {
+  packagerConfig: {
+    icon: './build/icon',
+    ignore: [
+      "\\.git",
+      "\\.ts",
+      "\\.tsx",
+    ]
   },
-  {
-    name: '@electron-forge/maker-zip',
-    platforms: ['darwin'],
-  },
-  {
-    name: '@electron-forge/maker-deb',
-    config: {},
-  },
-  {
-    name: '@electron-forge/maker-rpm',
-    config: {},
-  },
-];
-export const publishers = [
-  {
-    name: "@electron-forge/publisher-github",
-    config: {
-      repository: {
-        owner: "joaolucenalima",
-        name: "controle-estoque-electron"
+  rebuildConfig: {},
+  makers: [
+    {
+      name: '@electron-forge/maker-squirrel',
+      config: {
+        "setupIcon": "./build/icon.ico",
+        "setupExe": `controle-estoque.exe`,
       },
-      prerelease: false,
-      draft: true
+    },
+    {
+      name: '@electron-forge/maker-zip',
+      platforms: ['darwin'],
+    },
+    {
+      name: '@electron-forge/maker-deb',
+      config: {},
+    },
+    {
+      name: '@electron-forge/maker-rpm',
+      config: {},
+    },
+  ],
+  publishers: [
+    {
+      name: "@electron-forge/publisher-github",
+      config: {
+        repository: {
+          owner: "joaolucenalima",
+          name: "controle-estoque-electron"
+        },
+        prerelease: false,
+        draft: true
+      }
     }
-  }
-];
+  ],
+};
