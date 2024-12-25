510374d8eaf27a110f60c33d0e1a2a060295a99a
joaolucenalima <joaovitorlucena000@gmail.com>
2024-12-25 12:43:48 -0300

    remove password logic

--- src/renderer/index.tsx
+++ src/renderer/index.tsx
@@ -1,38 +1,20 @@
-import React, { useState } from "react"
-import ReactDOM from "react-dom/client"
+import React from "react";
+import ReactDOM from "react-dom/client";
 import { injectStyle } from "react-toastify/dist/inject-style";
 
-import { AppRoutes } from "./routes"
-import { ResponseProvider } from "./contexts/NotificationContext"
-import LoginPage from "./pages/LoginPage";
+import { ResponseProvider } from "./contexts/NotificationContext";
+import { AppRoutes } from "./routes";
 
 function App() {
-
-  const [logged, setLogged] = useState(0)
-
-  function setIsLogged() {
-    setLogged(1)
-  }
-
   injectStyle();
 
-  if (logged == 1) {
-    return (
-      <React.StrictMode>
-        <ResponseProvider>
-          <AppRoutes />
-        </ResponseProvider>
-      </React.StrictMode >
-    )
-  } else {
-    return (
-      <React.StrictMode>
-        <ResponseProvider>
-          <LoginPage setIsLogged={setIsLogged} />
-        </ResponseProvider>
-      </React.StrictMode >
-    )
-  }
+  return (
+    <React.StrictMode>
+      <ResponseProvider>
+        <AppRoutes />
+      </ResponseProvider>
+    </React.StrictMode >
+  )
 }
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
